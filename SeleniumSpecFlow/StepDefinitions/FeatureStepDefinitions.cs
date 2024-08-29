8410831bc36f0af6c6f2485d0dacef3342bcfe1e
chipi <chipineiro@gmail.com>
2024-08-28 21:24:39 -0600

    Create first Hook and move driver creation there

--- SeleniumSpecFlow/StepDefinitions/FeatureStepDefinitions.cs
+++ SeleniumSpecFlow/StepDefinitions/FeatureStepDefinitions.cs
@@ -1,5 +1,4 @@
 ﻿using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
 
 namespace SeleniumSpecFlow.StepDefinitions
 {
@@ -8,26 +7,28 @@ namespace SeleniumSpecFlow.StepDefinitions
     {
         private IWebDriver driver;
 
+        public FeatureStepDefinitions(IWebDriver driver) 
+        {
+            this.driver = driver;
+        }
+
         [Given(@"Open the browser")]
         public void GivenOpenTheBrowser()
         {
-            driver = new ChromeDriver();
-            driver.Manage().Window.Maximize();
         }
 
         [When(@"Enter the URL")]
         public void WhenEnterTheURL()
         {
             driver.Url = "https://youtube.com";
-            Thread.Sleep(5000);
         }
 
         [Then(@"Search for the Testers Talk")]
         public void ThenSearchForTheTestersTalk()
         {
-            driver.FindElement(By.Name("search_query")).SendKeys("Testers Talk" + Keys.Enter);
-            Thread.Sleep(5000);
-            driver.Quit();
+            driver.FindElement(By.XPath("//input[@id='search']")).SendKeys("Testers Talk");
+            driver.FindElement(By.XPath("//input[@id='search']")).SendKeys(Keys.Enter);
+            Thread.Sleep(3000);
         }
     }
 }
